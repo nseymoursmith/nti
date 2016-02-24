6e653c587b0bbfe23560649ff7474242a6ff2133
Nic <nseymoursmith@gmail.com>
2016-02-24 15:23:16 +0000

    moved save to before supply changing

--- inventory/models.py
+++ inventory/models.py
@@ -73,6 +73,7 @@ class StockOrder(models.Model):
     delivered = models.BooleanField('Delivered?', default = False)
     def save(self, *args, **kwargs):
         if not self.pk and self.delivered:
+            super(StockOrder, self).save(*args, **kwargs)
             for entry in self.items_ordered.all():
                 entry.supply += ItemOrder.objects.filter(stock_order__date=self.date).filter(item__name=entry.name)[0].number_ordered
                 entry.save()
@@ -87,7 +88,7 @@ class StockOrder(models.Model):
                     for entry in self.items_ordered.all():
                         entry.supply -= ItemOrder.objects.filter(stock_order__date=self.date).filter(item__name=entry.name)[0].number_ordered
                         entry.save()
-        super(StockOrder, self).save(*args, **kwargs)
+            super(StockOrder, self).save(*args, **kwargs)
         checkStock()
 
     def __unicode__(self):
